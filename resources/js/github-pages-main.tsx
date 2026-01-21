e5803daede372e70ceff30507893d51d0eafcc4a
12leandry <takafonick1122@gmail.com>
2026-01-21 08:46:30 +0100

    resoleve gh-pages

--- resources/js/github-pages-main.tsx
+++ resources/js/github-pages-main.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Landing from './pages/Landing';
 import '../css/app.css';
+import { LanguageProvider, ThemeProvider } from './hooks/useLanguageTheme';
+import Home from './pages/Home';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <Landing />
+        <LanguageProvider>
+            <ThemeProvider>
+                <Home />
+            </ThemeProvider>
+        </LanguageProvider>
     </React.StrictMode>
 );
